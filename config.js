c11f4bfb1fb23cb04e4b039766f6983ef972d016
LukaszWiszniewski <wisznu07@gmail.com>
2018-03-22 17:55:41 +0100

    Added google authentication without dynamic pages

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'GOOGLE_CLIENT_ID':'814801850613-fb3jccr4s3cua5tdtr06v0jhrc4ricub.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '2je8z0vcCuLEdUraS_5jajuh',
+    'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
