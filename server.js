c13d2fbfaa74ec9cf22d6c30c9b8353370a24b95
LukaszWiszniewski <wisznu07@gmail.com>
2018-03-22 15:03:52 +0100

    Added dynamic website

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(express.static('assets'));
 /*
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
 */
+app.get('/first-template', function (req, res) {
+    res.render('first-template');
+});
+app.get('/log-template', function (req, res) {
+    res.render('log-template');
+});
+app.get('/auth/google', function (req, res) {
+    const response = {
+        login: req.query.login,
+        password: req.query.password
+    };
+    //res.render(response);
+    res.end(JSON.stringify(response))
+});
 
 app.get('/', function (req, res) {
     res.sendFile('./index.html');
@@ -17,14 +34,17 @@ app.get('/userform', function (req, res) {
         first_name: req.query.first_name,
         last_name: req.query.last_name
     };
-    res.end(JSON.stringify(response));
+    res.end(JSON.stringify(response));                  //Co oznacza metoda end? jakie sa metody wyswietlania oprocz sendFile i render?
 });
 
-var server = app.listen(3000, 'localhost', function() {
+var server = app.listen(3000, 'localhost', function () {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
 
 /*server.on('request', function (request, response) {
