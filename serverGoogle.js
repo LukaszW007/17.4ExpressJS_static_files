f8fbec14b13839b761396ff1ead8b5c35b44ad7d
LukaszWiszniewski <wisznu07@gmail.com>
2018-03-22 20:46:56 +0100

    Fixed google authentication

--- serverGoogle.js
+++ serverGoogle.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var config = require('./config');
 var app = express();
 var googleProfile = {};
@@ -50,4 +50,11 @@ app.get('/auth/google/callback',
         failureRedirect: '/'
     }));
 
-app.listen(3000);
+/*app.get('/auth/google/callback',
+    passport.authenticate('google', {failureRedirect: '/login'}), function (req, res) {
+    // Successful authentication, redirect home.
+        res.redirect('/log-template');
+});*/
+
+
+    app.listen(3000);
